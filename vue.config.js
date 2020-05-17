9c2f0d2906d4c9fb1764f8356151d51b6877201e
Simon Schimmels <simon.schimmels@gmail.com>
2020-05-17 20:25:53 +0200

    Adapt publicPath for gh-pages deployment

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   lintOnSave: false,
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/flattenthecurve/' : '/',
 }
